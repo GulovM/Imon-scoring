383b943ad7db785807ebfad2cf2c1468b7600947
GulovM <147307957+GulovM@users.noreply.github.com>
2023-10-17 16:41:11 +0500

    Imon.py

--- Imon.py
+++ Imon.py
@@ -25,26 +25,26 @@ elif model_selected == 'RandomForestClassifier(without options)':
 
                      
                      
-def predict_note_authentication(Age, Issue_amount_nominal, Term, Age_full_years, Family_status, Type_of_client, Education, Tupe_of_business):
-    prediction=classifier.predict([[Age, Issue_amount_nominal, Term, Age_full_years, Family_status, Type_of_client, Education, Tupe_of_business]])
+def predict_note_authentication(gender, Issue_amount_nominal, Term, age, Family_status, Type_of_client, education, Tupe_of_business):
+    prediction=classifier.predict([[gender, Issue_amount_nominal, Term, age, Family_status, Type_of_client, education, Tupe_of_business]])
     print(prediction)
     return prediction
                      
                      
 def main():
     st.title("Imon International's Bank scoring system")
-    Age = st.radio('Ваш пол?(0 - male, 1 - female)', (0, 1))
+    gender = st.radio('Ваш пол?(0 - male, 1 - female)', (0, 1))
     Issue_amount_nominal = st.number_input('Какова сумма выдочи номинала(используйте только цифры)?', step=1, value=0)
     Term = st.number_input('На какой срок вы хотите кредит?(используйте только цифры)?', step=1, value=0) 
-    Age_full_years = st.number_input('Сколько вам полных лет?(используйте только цифры)?', step=1, value=0)
+    age = st.number_input('Сколько вам полных лет?(используйте только цифры)?', step=1, value=0)
     Family_status = st.radio('Каков ваш семеный статус?(0 - Widow/Widower, 1 - Single, 2 - Married, 3 - Divorced)', (0, 1, 2, 3))
     Type_of_client = st.radio('Какой вы клиент?(0 - Старый клиент, 1 - Новый клиент)', (0, 1))    
-    Education = st.radio('Какое у вас образование?(0 - Высшее образование, 1 - Сред.спец.образ-ние, 2 - Среднее образование, 3 - Непол Сред.образ, 4 - Начал образование, 5 - Аспирантура)', (0, 1, 2, 3, 4, 5))
+    education = st.radio('Какое у вас образование?(0 - Высшее образование, 1 - Сред.спец.образ-ние, 2 - Среднее образование, 3 - Непол Сред.образ, 4 - Начал образование, 5 - Аспирантура)', (0, 1, 2, 3, 4, 5))
     Tupe_of_business = st.radio('Какой у вас вид бизнеса?(0 - 1. Карзи истеъмоли/Потребительский кредит, 1 - 2. Истехсолот/Производство, 2 - 6. Хочагии кишлок / Сельское хозяйство, 3 - 3. Хизматрасони/Услуги, 4 - 4. Савдо / Торговля)', (0, 1, 2, 3, 4)) 
                      
     result=""
     if st.button("Predict"):
-        result=int(predict_note_authentication(Age, Issue_amount_nominal, Term, Age_full_years, Family_status, Type_of_client, Education, Tupe_of_business)) 
+        result=int(predict_note_authentication(gender, Issue_amount_nominal, Term, age, Family_status, Type_of_client, education, Tupe_of_business)) 
      #st.success('The output is {}'.format(result))
     st.success('Scoring system result is(1 - Длительность самой долгой единовременной просрочки в течение цикла > 20, 0 - Scoring system result is(1 - Длительность самой долгой единовременной просрочки в течение цикла <= 20) {}'.format(result))
                      
