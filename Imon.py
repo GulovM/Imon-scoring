df6fdd4948d08800006b7a75b6322ce1823c33c7
GulovM <147307957+GulovM@users.noreply.github.com>
2023-10-17 17:52:57 +0500

    Imon.py

--- Imon.py
+++ Imon.py
@@ -1,5 +1,8 @@
-import streamlit as st
+import numpy as np
 import pickle
+import pandas as pd
+import streamlit as st
+
 
 st.subheader('Imon International Bank Scoring')
 
@@ -18,26 +21,35 @@ elif model_selected == 'RandomForestClassifier(without options)':
     pickle_in = open("Forest.pkl","rb")
     classifier=pickle.load(pickle_in)
 
-def predict_score(gender, Issue_amount_nominal, Term, age, Family_status, Type_of_client, education, Tupe_of_business):
-    prediction = model.predict([[gender, Issue_amount_nominal, Term, age, Family_status, Type_of_client, education, Tupe_of_business]])[0]
+                     
+                     
+def predict_note_authentication((gender, Issue_amount_nominal, Term, age, Family_status, Type_of_client, education, Tupe_of_business)):
+    prediction=classifier.predict([[(gender, Issue_amount_nominal, Term, age, Family_status, Type_of_client, education, Tupe_of_business)]])
+    print(prediction)
     return prediction
-
+                     
+                     
 def main():
-    st.title("Imon International's Bank Scoring System")
-    gender = st.radio('Ваш пол?(0 - мужской, 1 - женский)', (0, 1))
-    Issue_amount_nominal = st.number_input('Сумма выдачи номинал (только числа)', step=1, value=0)
-    Term = st.number_input('Срок кредита (только числа)', step=1, value=0) 
-    age = st.number_input('Возраст (только числа)', step=1, value=0)
-    Family_status = st.radio('Семейное положение (0 - вдовец/вдова, 1 - холост/не замужем, 2 - женат/замужем, 3 - разведен(а))', (0, 1, 2, 3))
-    Type_of_client = st.radio('Тип клиента (0 - старый клиент, 1 - новый клиент)', (0, 1))    
-    education = st.radio('Образование (0 - высшее, 1 - среднее специальное, 2 - среднее, 3 - неполное среднее, 4 - начальное, 5 - аспирантура)', (0, 1, 2, 3, 4, 5))
-    Tupe_of_business = st.radio('Вид бизнеса (0 - 1. Потребительский кредит, 1 - 2. Производство, 2 - 6. Сельское хозяйство, 3 - 3. Услуги, 4 - 4. Торговля)', (0, 1, 2, 3, 4)) 
-
-if st.button("Предсказать"):
-    prediction = predict_score(gender, Issue_amount_nominal, Term, age, Family_status, Type_of_client, education, Tupe_of_business)
-    st.success(f'Результат скоринговой системы: {prediction}')
-else:
-    st.warning("Выберите модель для загрузки.")
-
-if __name__ == '__main__':
+    st.title("Imon International's Bank scoring system")
+    gender = st.radio('Ваш пол?(0 - male, 1 - female)', (0, 1))
+    Issue_amount_nominal = st.number_input('Какова сумма выдочи номинала(используйте только цифры)?', step=1, value=0)
+    Term = st.number_input('На какой срок вы хотите кредит?(используйте только цифры)?', step=1, value=0) 
+    age = st.number_input('Сколько вам полных лет?(используйте только цифры)?', step=1, value=0)
+    Family_status = st.radio('Каков ваш семеный статус?(0 - Widow/Widower, 1 - Single, 2 - Married, 3 - Divorced)', (0, 1, 2, 3))
+    Type_of_client = st.radio('Какой вы клиент?(0 - Старый клиент, 1 - Новый клиент)', (0, 1))    
+    education = st.radio('Какое у вас образование?(0 - Высшее образование, 1 - Сред.спец.образ-ние, 2 - Среднее образование, 3 - Непол Сред.образ, 4 - Начал образование, 5 - Аспирантура)', (0, 1, 2, 3, 4, 5))
+    Tupe_of_business = st.radio('Какой у вас вид бизнеса?(0 - 1. Карзи истеъмоли/Потребительский кредит, 1 - 2. Истехсолот/Производство, 2 - 6. Хочагии кишлок / Сельское хозяйство, 3 - 3. Хизматрасони/Услуги, 4 - 4. Савдо / Торговля)', (0, 1, 2, 3, 4)) 
+                     
+    result=""
+    if st.button("Predict"):
+        result=int(predict_note_authentication(gender, Issue_amount_nominal, Term, age, Family_status, Type_of_client, Education, Tupe_of_business)) 
+     #st.success('The output is {}'.format(result))
+    st.success('Scoring system result is(1 - Длительность самой долгой единовременной просрочки в течение цикла > 15, 0 - Scoring system result is(1 - Длительность самой долгой единовременной просрочки в течение цикла <= 15) {}'.format(result))
+                     
+    
+              
+                  
+if __name__=='__main__':
     main()
+
+   
