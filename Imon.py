c22c8b4a2d8d3ca94c9c8ab8c67e112d88917498
GulovM <147307957+GulovM@users.noreply.github.com>
2023-10-17 17:02:19 +0500

    Imon.py

--- Imon.py
+++ Imon.py
@@ -39,24 +39,23 @@ def main():
     if classifier is None:
         return
     
-    gender = st.radio('Ваш пол?(0 - male, 1 - female)', (0, 1))
-    Issue_amount_nominal = st.number_input('Какова сумма выдочи номинала(используйте только цифры)?', step=1, value=0)
-    Term = st.number_input('На какой срок вы хотите кредит?(используйте только цифры)?', step=1, value=0) 
-    age = st.number_input('Сколько вам полных лет?(используйте только цифры)?', step=1, value=0)
-    Family_status = st.radio('Каков ваш семеный статус?(0 - Widow/Widower, 1 - Single, 2 - Married, 3 - Divorced)', (0, 1, 2, 3))
-    Type_of_client = st.radio('Какой вы клиент?(0 - Старый клиент, 1 - Новый клиент)', (0, 1))    
-    education = st.radio('Какое у вас образование?(0 - Высшее образование, 1 - Сред.спец.образ-ние, 2 - Среднее образование, 3 - Непол Сред.образ, 4 - Начал образование, 5 - Аспирантура)', (0, 1, 2, 3, 4, 5))
-    Tupe_of_business = st.radio('Какой у вас вид бизнеса?(0 - 1. Карзи истеъмоли/Потребительский кредит, 1 - 2. Истехсолот/Производство, 2 - 6. Хочагии кишлок / Сельское хозяйство, 3 - 3. Хизматрасони/Услуги, 4 - 4. Савдо / Торговля)', (0, 1, 2, 3, 4)) 
-                     
-   result = ""
-    if st.button("Predict"):
-        prediction = predict_note_authentication(gender, Issue_amount_nominal, Term, age, Family_status, Type_of_client, education, Tupe_of_business, classifier)
-        if prediction is not None:
-            result = int(prediction[0])
-    
-    st.success(f'Scoring system result is {result}')
+gender = st.radio('Ваш пол?(0 - male, 1 - female)', (0, 1))
+Issue_amount_nominal = st.number_input('Какова сумма выдочи номинала(используйте только цифры)?', step=1, value=0)
+Term = st.number_input('На какой срок вы хотите кредит?(используйте только цифры)?', step=1, value=0) 
+age = st.number_input('Сколько вам полных лет?(используйте только цифры)?', step=1, value=0)
+Family_status = st.radio('Каков ваш семеный статус?(0 - Widow/Widower, 1 - Single, 2 - Married, 3 - Divorced)', (0, 1, 2, 3))
+Type_of_client = st.radio('Какой вы клиент?(0 - Старый клиент, 1 - Новый клиент)', (0, 1))    
+education = st.radio('Какое у вас образование?(0 - Высшее образование, 1 - Сред.спец.образ-ние, 2 - Среднее образование, 3 - Непол Сред.образ, 4 - Начал образование, 5 - Аспирантура)', (0, 1, 2, 3, 4, 5))
+Tupe_of_business = st.radio('Какой у вас вид бизнеса?(0 - 1. Карзи истеъмоли/Потребительский кредит, 1 - 2. Истехсолот/Производство, 2 - 6. Хочагии кишлок / Сельское хозяйство, 3 - 3. Хизматрасони/Услуги, 4 - 4. Савдо / Торговля)', (0, 1, 2, 3, 4)) 
+             
+result = ""
+if st.button("Predict"):
+    prediction = predict_note_authentication(gender, Issue_amount_nominal, Term, age, Family_status, Type_of_client, education, Tupe_of_business, classifier)
+    if prediction is not None:
+        result = int(prediction[0])
+
+st.success(f'Scoring system result is {result}')
 
 if __name__ == '__main__':
     main()
-
    
